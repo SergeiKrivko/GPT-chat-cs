4531e1a2126077994613d0fe8ef4c0922d2ca0d5
SergeiKrivko <sergik.krivko@gmail.com>
2024-09-11 02:15:27 +0300

    Chat settings

---
+++ Core/RemoteRepository/GptHttpService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using Auth;
+using Core.RemoteRepository.Models;
+using Utils;
+using Utils.Http;
+using Utils.Http.Exceptions;
+
+namespace Core.RemoteRepository;
+
+public class GptHttpService: BodyDetailHttpService
+{
+    private static GptHttpService? _instance;
+
+    public static GptHttpService Instance
+    {
+        get
+        {
+            _instance ??= new GptHttpService();
+            return _instance;
+        }
+    }
+
+    private GptHttpService()
+    {
+        BaseUrl = Config.BaseUrl;
+    }
+
+    public async Task<List<string>> GetModels()
+    {
+        try
+        {
+            return await Get<List<string>>("/api/v1/gpt/models");
+        }
+        catch (NotFoundException)
+        {
+            return new();
+        }
+    }
+}
--- GptChat/GptChat/Views/ChatSettings.axaml.cs
+++ GptChat/GptChat/Views/ChatSettings.axaml.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Core;
+using Core.RemoteRepository;
+using Utils.Http.Exceptions;
 
 namespace GptChat.Views;
 
 public partial class ChatSettings : UserControl
 {
     public Chat? Chat { get; set; }
+    private List<string> Models { get; } = new();
     
     public ChatSettings()
     {
         InitializeComponent();
+        GetModels();
+    }
+
+    private async void GetModels()
+    {
+        while (true)
+        {
+            try
+            {
+                Models.AddRange(await GptHttpService.Instance.GetModels());
+                ChatModelBox.ItemsSource = Models;
+                break;
+            }
+            catch (ConnectionException)
+            {
+            }
+        }
     }
 
     public void Update()
@@ -20,6 +42,8 @@ public partial class ChatSettings : UserControl
             return;
         ChatNameBox.Text = Chat.Name;
         ChatModelBox.SelectedItem = Chat.Model;
+        ContextBox.Value = Chat.ContextSize;
+        TemperatureBox.Value = (decimal)Chat.Temperature;
     }
 
     public void Save()
@@ -27,13 +51,28 @@ public partial class ChatSettings : UserControl
         if (Chat == null)
             return;
         var flag = false;
+        Console.WriteLine(ChatModelBox.SelectedValue);
         flag |= Chat.Name != ChatNameBox.Text;
-        // flag |= Chat.Model != (string?)ChatModelBox.SelectedValue;
+        flag |= Chat.Model != (string?)ChatModelBox.SelectedValue;
+        flag |= Chat.ContextSize != ContextBox.Value;
+        flag |= Chat.Temperature != (double?)TemperatureBox.Value;
         if (flag)
         {
             Chat.Name = ChatNameBox.Text ?? "";
-            // Chat.Model = (string?)ChatModelBox.SelectedValue;
+            Chat.Model = (string?)ChatModelBox.SelectedValue;
+            Chat.ContextSize = (int?)ContextBox.Value ?? 0;
+            Chat.Temperature = (double?)TemperatureBox.Value ?? 0.5;
             ChatsService.Instance.SaveChat(Chat);
         }
+    }
+
+    private void Save_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Save();
+    }
+
+    private void Cancel_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Update();
     }
 }
